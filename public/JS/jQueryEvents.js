eecc859fbb521ddfd9f906246fb6b7a13f9250ac
Evan Lim <evanlim@Evan-Lims-MacBook-Pro.local>
2015-03-26 19:00:18 -0400

    completed map markers and side panel element appends

--- public/JS/jQueryEvents.js
+++ public/JS/jQueryEvents.js
@@ -1,14 +1,22 @@
-$(document).ready(function(){
-
+$(document).ready(function() {
 
 var existingHotelMarker;
+var existingRestaurantMarkers = [];
+var existingToDoMarkers = [];
+
+function updateMarkers (max, current, neo) {   // current is an array of current markers
+  											// new is the new marker to add to the map
+}
 
 $("#top-panel .hotel").on("click", 'button' , function(){
 	var value = $('#hotelSelect').val();
-	console.log(value);
 	var item = _.where(all_hotels, {_id: value})[0];
-	console.log(item);
-	$('.hotel .title')[0].firstChild.data = item.name;
+	var element = $('<div class="itinerary-item"><span class="title">' + item.name + '</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>');
+	if ($('ul.hotel').children().length == 1) {
+		$('ul.hotel').children()[0].remove();
+	}
+	$('ul.hotel').append(element);
+
 	//debugger;
 	var location = item.place[0].location;
 	//Define map location
@@ -23,28 +31,50 @@ $("#top-panel .hotel").on("click", 'button' , function(){
 	marker.setMap(map);
 });
 
-
 $('#top-panel .restaurant').on('click', 'button', function() {
 
-    var value = $(this).parent().find('select :selected').val();
-
-    if ($('ul.restaurant').children().length > 0 && $('ul.restaurant').children().length < 3) {
-      var element = $('<div class="itinerary-item"><span class="title">' + value + '</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>');
-
+    //var value = $(this).parent().find('select :selected');
+    var value = $('#restaurantSelect').val();
+    var item = _.where(all_restaurants, {_id: value})[0];
+    if ($('ul.restaurant').children().length < 3) {
+      var element = $('<div class="itinerary-item"><span class="title">' + item.name + '</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>');
         $('ul.restaurant').append(element);
     }
-
+    var location = item.place[0].location;
+	//Define map location
+	var loc = new google.maps.LatLng(location[0],location[1]);
+	marker = new google.maps.Marker({
+		position: loc,
+	//	map: map,
+		title: "Something"
+	})
+	if (existingRestaurantMarkers.length < 3) { 
+		marker.setMap(map); 
+		existingRestaurantMarkers.push(marker);
+	}
 })
 
 $('#top-panel .ttdo').on('click', 'button', function() {
 
-    var value = $(this).parent().find('select :selected').val();
-
-    if ($('ul.ttdo').children().length > 0 && $('ul.ttdo').children().length < 3) {
-      var element = $('<div class="itinerary-item"><span class="title">' + value + '</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>');
+    var value = $('#ttdoSelect').val();
+    var item = _.where(all_things_to_do, {_id: value})[0];
+    if ($('ul.ttdo').children().length < 3) {
+      var element = $('<div class="itinerary-item"><span class="title">' + item.name + '</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>');
 
         $('ul.ttdo').append(element);
     }
+        var location = item.place[0].location;
+	//Define map location
+	var loc = new google.maps.LatLng(location[0],location[1]);
+	marker = new google.maps.Marker({
+		position: loc,
+	//	map: map,
+		title: "Something"
+	})
+	if (existingToDoMarkers.length < 3) { 
+		marker.setMap(map); 
+		existingToDoMarkers.push(marker);
+	}
 
 });
 
